47289a89594eb2e11d84c884a3d5f7d5aa0cabd0
Pixel <89741457+PixelIndieDev@users.noreply.github.com>
2025-10-14 20:28:58 +0200

    Add files via upload

---
+++ 1.20-1.20.6/src/main/java/com/pixelindiedev/lazy_ai_pixelindiedev/mixin/AttackGoalMixin.java
@@ -0,0 +1,65 @@
+package com.pixelindiedev.lazy_ai_pixelindiedev.mixin;
+
+import com.pixelindiedev.lazy_ai_pixelindiedev.Lazy_ai_pixelindiedev;
+import com.pixelindiedev.lazy_ai_pixelindiedev.config.DistanceType;
+import net.minecraft.entity.ai.goal.AttackGoal;
+import net.minecraft.entity.mob.MobEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(value = AttackGoal.class, priority = 1001)
+public class AttackGoalMixin {
+    private final static int[] cooldowns = {10, 25, 50};  // Cooldowns from close to far, in ticks
+    private final static int[] cooldownsAgressive = {15, 30, 60};
+    private final static int[] cooldownsMinimal = {5, 15, 40};
+    @Unique
+    private MobEntity mob;
+    @Unique
+    private int cooldown = 0;
+    @Unique
+    private DistanceType previousDistanceType = DistanceType.FarRange;
+    @Unique
+    private int[] temparray = new int[3];
+
+    @Inject(method = "<init>", at = @At("RETURN"))
+    private void captureMob(MobEntity mob, CallbackInfo ci) {
+        this.mob = mob;
+    }
+
+    @Inject(method = "canStart", at = @At("HEAD"), cancellable = true)
+    private void throttleCanStart(CallbackInfoReturnable<Boolean> cir) {
+        DistanceType newDistanceType = Lazy_ai_pixelindiedev.getDistance(mob);
+
+        temparray = getCooldownList();
+        if (newDistanceType != previousDistanceType) {
+            cooldown = temparray[newDistanceType.ordinal()] - (temparray[previousDistanceType.ordinal()] - cooldown);
+            previousDistanceType = newDistanceType;
+        }
+
+        if (cooldown > 0) {
+            cooldown--;
+            cir.setReturnValue(false);
+        } else {
+            cooldown = temparray[newDistanceType.ordinal()];
+        }
+    }
+
+    @Inject(method = "tick", at = @At("HEAD"), cancellable = true)
+    private void throttleTick(CallbackInfo ci) {
+        if (temparray == null) return;
+
+        if ((mob.age + mob.getId()) % temparray[2] != 0) ci.cancel();
+    }
+
+    private int[] getCooldownList() {
+        return switch (Lazy_ai_pixelindiedev.getOptimalizationType()) {
+            case Minimal -> cooldownsMinimal;
+            case Agressive -> cooldownsAgressive;
+            default -> cooldowns;
+        };
+    }
+}
---
+++ 1.20-1.20.6/src/main/java/com/pixelindiedev/lazy_ai_pixelindiedev/mixin/DestroyEggGoalMixin.java
@@ -0,0 +1,63 @@
+package com.pixelindiedev.lazy_ai_pixelindiedev.mixin;
+
+import com.pixelindiedev.lazy_ai_pixelindiedev.Lazy_ai_pixelindiedev;
+import com.pixelindiedev.lazy_ai_pixelindiedev.config.DistanceType;
+import net.minecraft.block.Block;
+import net.minecraft.entity.ai.goal.StepAndDestroyBlockGoal;
+import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.entity.mob.PathAwareEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(value = StepAndDestroyBlockGoal.class, priority = 1001)
+public class DestroyEggGoalMixin {
+    private final static int[] cooldowns = {40, 100, 200};  // Cooldowns from close to far, in ticks
+    private final static int[] cooldownsAgressive = {60, 150, 300};
+    private final static int[] cooldownsMinimal = {5, 15, 50};
+    @Unique
+    private int cooldown = 0;
+    @Unique
+    private MobEntity mob;
+    @Unique
+    private DistanceType previousDistanceType = DistanceType.FarRange;
+
+    @Inject(method = "<init>", at = @At("RETURN"))
+    private void captureMob(Block targetBlock, PathAwareEntity mob, double speed, int maxYDifference, CallbackInfo ci) {
+        this.mob = mob;
+    }
+
+    @Inject(method = "canStart", at = @At("HEAD"), cancellable = true)
+    private void ThrottleEggCheck(CallbackInfoReturnable<Boolean> cir) {
+        if (Lazy_ai_pixelindiedev.getDisableZombieEggStomping()) {
+            cir.setReturnValue(false);
+            return;
+        }
+
+        DistanceType newDistanceType = Lazy_ai_pixelindiedev.getDistance(mob);
+
+        int[] temparray = getCooldownList();
+        if (newDistanceType != previousDistanceType) {
+            cooldown = temparray[newDistanceType.ordinal()] - (temparray[previousDistanceType.ordinal()] - cooldown);
+            previousDistanceType = newDistanceType;
+        }
+
+        if (cooldown > 0) {
+            cooldown--;
+            cir.setReturnValue(false);
+        } else {
+            cooldown = temparray[newDistanceType.ordinal()];
+        }
+    }
+
+    private int[] getCooldownList() {
+        return switch (Lazy_ai_pixelindiedev.getOptimalizationType()) {
+            case Minimal -> cooldownsMinimal;
+            case Agressive -> cooldownsAgressive;
+            default -> cooldowns;
+        };
+    }
+}
