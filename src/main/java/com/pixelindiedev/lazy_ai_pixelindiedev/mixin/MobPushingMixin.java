2e6534be973bc305bb47a73a8ba5abefcf47fc1f
Pixel <89741457+PixelIndieDev@users.noreply.github.com>
2025-12-22 16:56:11 +0100

    Add files via upload

--- src/main/java/com/pixelindiedev/lazy_ai_pixelindiedev/mixin/MobPushingMixin.java
+++ src/main/java/com/pixelindiedev/lazy_ai_pixelindiedev/mixin/MobPushingMixin.java
@@ -1,39 +1,73 @@
 package com.pixelindiedev.lazy_ai_pixelindiedev.mixin;
 
-import com.pixelindiedev.lazy_ai_pixelindiedev.config.OptimalizationType;
+import com.pixelindiedev.lazy_ai_pixelindiedev.Lazy_ai_pixelindiedev;
+import com.pixelindiedev.lazy_ai_pixelindiedev.config.DistanceType;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import static com.pixelindiedev.lazy_ai_pixelindiedev.Lazy_ai_pixelindiedev.getDistance;
-import static com.pixelindiedev.lazy_ai_pixelindiedev.Lazy_ai_pixelindiedev.getOptimalizationType;
+import static com.pixelindiedev.lazy_ai_pixelindiedev.Lazy_ai_pixelindiedev.EnableCriticalTPSMode;
 
 @Mixin(LivingEntity.class)
-public class MobPushingMixin {
+public abstract class MobPushingMixin {
+    private final static int[] cooldowns = {5, 10, 15};  // Cooldowns from close to far, in ticks
+    private final static int[] cooldownsAgressive = {10, 15, 25};
+    private final static int[] cooldownsMinimal = {2, 6, 10};
     @Unique
-    private boolean shouldDisable() {
-        LivingEntity entity = (LivingEntity) (Object) this;
+    private int cooldown = 0;
+    @Unique
+    private MobEntity mob;
+    @Unique
+    private DistanceType previousDistanceType = DistanceType.FarRange;
 
-        if (entity instanceof PlayerEntity) return false;
-        if (entity.getAttacking() != null || entity.getLastAttacker() != null) return false;
-
-        return switch (getDistance((MobEntity) entity)) {
-            case MediumRange -> getOptimalizationType() == OptimalizationType.Agressive;
-            case FarRange -> getOptimalizationType() != OptimalizationType.Minimal;
-            default -> false;
-        };
+    @Inject(method = "<init>", at = @At("RETURN"))
+    private void captureMob(EntityType entityType, World world, CallbackInfo ci) {
+        this.mob = GetMobEntity();
     }
 
     @Inject(method = "pushAwayFrom", at = @At("HEAD"), cancellable = true)
     private void ThrottlePush(Entity other, CallbackInfo ci) {
-        if (shouldDisable()) {
-            ci.cancel();
+        if (EnableCriticalTPSMode) ci.cancel();
+
+        if (mob != null) {
+            DistanceType newDistanceType = Lazy_ai_pixelindiedev.getDistance(mob);
+
+            int[] temparray = getCooldownList();
+            if (newDistanceType != previousDistanceType) {
+                cooldown = temparray[newDistanceType.ordinal()] - (temparray[previousDistanceType.ordinal()] - cooldown);
+                previousDistanceType = newDistanceType;
+            }
+
+            if (cooldown > 0) {
+                cooldown--;
+                ci.cancel();
+            } else {
+                cooldown = temparray[newDistanceType.ordinal()];
+            }
         }
+    }
+
+    @Unique
+    private MobEntity GetMobEntity() {
+        LivingEntity entity = (LivingEntity) (Object) this;
+
+        if (entity instanceof PlayerEntity) return null;
+        else return (MobEntity) entity;
+    }
+
+    private int[] getCooldownList() {
+        return switch (Lazy_ai_pixelindiedev.getOptimalizationType()) {
+            case Minimal -> cooldownsMinimal;
+            case Agressive -> cooldownsAgressive;
+            default -> cooldowns;
+        };
     }
 }
