d40659aa84c06217958a8c60d742926581d11fa7
Pixel <89741457+PixelIndieDev@users.noreply.github.com>
2026-02-01 16:29:55 +0100

    Add files via upload

---
+++ src/main/java/com/pixelindiedev/lazy_ai_pixelindiedev/LazyAI$BlockChecker.java
@@ -0,0 +1,118 @@
+package com.pixelindiedev.lazy_ai_pixelindiedev;
+
+import it.unimi.dsi.fastutil.objects.Reference2BooleanMap;
+import it.unimi.dsi.fastutil.objects.Reference2BooleanOpenHashMap;
+import net.minecraft.block.*;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+
+public class LazyAI$BlockChecker {
+    //this checks for collision on blocks more efficiently than the isSolidBlock() as this checks less accurate, although I don't need the accuracy for what I use it for
+    private static final Reference2BooleanMap<Block> blockSolidCollisionCheckCache = new Reference2BooleanOpenHashMap<>(); //Stays in memory, does not have a big size, so it's fine
+    private static volatile boolean initialized = false;
+
+    static {
+        blockSolidCollisionCheckCache.defaultReturnValue(false);
+    }
+
+    public static void initializeCacheAsync() {
+        new Thread(() -> {
+            initializeCache();
+            System.out.println("LazyAI$BlockCheckerCacheInit started initializing asynchronously");
+        }, "LazyAI$BlockCheckerCacheInit").start();
+    }
+
+    private static void initializeCache() {
+        if (initialized) return;
+
+        synchronized (blockSolidCollisionCheckCache) {
+            if (initialized) return;
+            //default list
+            //to reduce checks during runtime
+
+            //liquids
+            blockSolidCollisionCheckCache.put(Blocks.AIR, false);
+            blockSolidCollisionCheckCache.put(Blocks.CAVE_AIR, false);
+            blockSolidCollisionCheckCache.put(Blocks.VOID_AIR, false);
+            blockSolidCollisionCheckCache.put(Blocks.WATER, false);
+            blockSolidCollisionCheckCache.put(Blocks.LAVA, false);
+
+            //common terrain
+            blockSolidCollisionCheckCache.put(Blocks.STONE, true);
+            blockSolidCollisionCheckCache.put(Blocks.DEEPSLATE, true);
+            blockSolidCollisionCheckCache.put(Blocks.GRANITE, true);
+            blockSolidCollisionCheckCache.put(Blocks.DIORITE, true);
+            blockSolidCollisionCheckCache.put(Blocks.ANDESITE, true);
+            blockSolidCollisionCheckCache.put(Blocks.DIRT, true);
+            blockSolidCollisionCheckCache.put(Blocks.GRASS_BLOCK, true);
+            blockSolidCollisionCheckCache.put(Blocks.COARSE_DIRT, true);
+            blockSolidCollisionCheckCache.put(Blocks.PODZOL, true);
+            blockSolidCollisionCheckCache.put(Blocks.MYCELIUM, true);
+            blockSolidCollisionCheckCache.put(Blocks.SAND, true);
+            blockSolidCollisionCheckCache.put(Blocks.RED_SAND, true);
+            blockSolidCollisionCheckCache.put(Blocks.GRAVEL, true);
+            blockSolidCollisionCheckCache.put(Blocks.CLAY, true);
+            blockSolidCollisionCheckCache.put(Blocks.COBBLESTONE, true);
+
+            //wood
+            blockSolidCollisionCheckCache.put(Blocks.OAK_PLANKS, true);
+            blockSolidCollisionCheckCache.put(Blocks.SPRUCE_PLANKS, true);
+            blockSolidCollisionCheckCache.put(Blocks.BIRCH_PLANKS, true);
+            blockSolidCollisionCheckCache.put(Blocks.JUNGLE_PLANKS, true);
+            blockSolidCollisionCheckCache.put(Blocks.ACACIA_PLANKS, true);
+            blockSolidCollisionCheckCache.put(Blocks.DARK_OAK_PLANKS, true);
+
+            //common non-solid
+            blockSolidCollisionCheckCache.put(Blocks.GRASS, false);
+            blockSolidCollisionCheckCache.put(Blocks.TALL_GRASS, false);
+            blockSolidCollisionCheckCache.put(Blocks.FERN, false);
+            blockSolidCollisionCheckCache.put(Blocks.LARGE_FERN, false);
+            blockSolidCollisionCheckCache.put(Blocks.DEAD_BUSH, false);
+            blockSolidCollisionCheckCache.put(Blocks.SEAGRASS, false);
+            blockSolidCollisionCheckCache.put(Blocks.TALL_SEAGRASS, false);
+            blockSolidCollisionCheckCache.put(Blocks.KELP, false);
+            blockSolidCollisionCheckCache.put(Blocks.KELP_PLANT, false);
+            blockSolidCollisionCheckCache.put(Blocks.DANDELION, false);
+            blockSolidCollisionCheckCache.put(Blocks.POPPY, false);
+            blockSolidCollisionCheckCache.put(Blocks.TORCH, false);
+            blockSolidCollisionCheckCache.put(Blocks.WALL_TORCH, false);
+            blockSolidCollisionCheckCache.put(Blocks.REDSTONE_TORCH, false);
+            blockSolidCollisionCheckCache.put(Blocks.REDSTONE_WIRE, false);
+            blockSolidCollisionCheckCache.put(Blocks.FIRE, false);
+            blockSolidCollisionCheckCache.put(Blocks.SOUL_FIRE, false);
+            blockSolidCollisionCheckCache.put(Blocks.SNOW, false);
+
+            initialized = true;
+            System.out.println("LazyAI$BlockCheckerCacheInit finished initializing asynchronously");
+        }
+    }
+
+    public static boolean hasSolidCollision(Block block) {
+        if (blockSolidCollisionCheckCache.containsKey(block)) return blockSolidCollisionCheckCache.getBoolean(block);
+        boolean isSolid = hasCollisionFast(block);
+        blockSolidCollisionCheckCache.put(block, isSolid);
+        return isSolid;
+    }
+
+    public static boolean hasSolidCollision(BlockState state) {
+        return hasSolidCollision(state.getBlock());
+    }
+
+    private static boolean hasCollisionFast(Block block) {
+        if (blockSolidCollisionCheckCache.containsKey(block)) return blockSolidCollisionCheckCache.getBoolean(block);
+
+        //these should have a different boolean value
+        //is it a door or trapdoor
+        if (block instanceof DoorBlock || block instanceof TrapdoorBlock) {
+            blockSolidCollisionCheckCache.put(block, false);
+            return false;
+        }
+
+        BlockState defaultState = block.getDefaultState();
+        VoxelShape collisionShape = defaultState.getCollisionShape(null, null);
+        boolean isSolid = !collisionShape.isEmpty() && collisionShape != VoxelShapes.empty();
+
+        blockSolidCollisionCheckCache.put(block, isSolid);
+        return isSolid;
+    }
+}
--- src/main/java/com/pixelindiedev/lazy_ai_pixelindiedev/mixin/VillagerEntityMixin.java
+++ src/main/java/com/pixelindiedev/lazy_ai_pixelindiedev/mixin/VillagerEntityMixin.java
@@ -1,7 +1,9 @@
 package com.pixelindiedev.lazy_ai_pixelindiedev.mixin;
 
 import com.pixelindiedev.lazy_ai_pixelindiedev.Lazy_ai_pixelindiedev;
+import com.pixelindiedev.lazy_ai_pixelindiedev.interfaces.VillagerCacheAccessor;
 import com.pixelindiedev.lazy_ai_pixelindiedev.mixin.integration.VillagerEntityAccessor;
+import it.unimi.dsi.fastutil.longs.Long2BooleanOpenHashMap;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.EntityInteraction;
 import net.minecraft.entity.EntityType;
@@ -12,6 +14,7 @@ import net.minecraft.entity.passive.VillagerEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.village.VillagerProfession;
 import net.minecraft.village.VillagerType;
 import net.minecraft.world.World;
@@ -22,21 +25,35 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(VillagerEntity.class)
-public abstract class VillagerEntityMixin {
-    @Unique
-    private final static int[] cooldowns = {20, 50, 100};  // Cooldowns from close to far, in ticks
-    @Unique
-    private final static int[] cooldownsAgressive = {40, 75, 200};
-    @Unique
-    private final static int[] cooldownsMinimal = {10, 30, 60};
+import static com.pixelindiedev.lazy_ai_pixelindiedev.LazyAI$BlockChecker.hasSolidCollision;
+import static com.pixelindiedev.lazy_ai_pixelindiedev.Lazy_ai_pixelindiedev.getOptimalizationType;
 
+@Mixin(VillagerEntity.class)
+public abstract class VillagerEntityMixin implements VillagerCacheAccessor {
+    @Unique
+    private final static int[] cooldowns = {50, 90, 150};  // Cooldowns from close to far, in ticks
+    @Unique
+    private final static int[] cooldownsAgressive = {70, 115, 250};
+    @Unique
+    private final static int[] cooldownsMinimal = {30, 70, 110};
+    @Unique
+    private final Long2BooleanOpenHashMap cachedBlockPos = new Long2BooleanOpenHashMap(9);
+    @Unique
+    private final Direction[] directionsDirections = {Direction.NORTH, Direction.SOUTH, Direction.EAST, Direction.WEST};
+    @Unique
+    private BlockPos.Mutable reusableSide = new BlockPos.Mutable();
     @Unique
     private VillagerEntity villager;
     @Unique
     private boolean isInTradingHall;
     @Unique
+    private boolean shouldRefreshTradingHall;
+    @Unique
+    private BlockPos lastStandingLocation;
+    @Unique
     private int randomSelectedTick;
+    @Unique
+    private VillagerProfession cachedProfessionEntry;
 
     @Shadow
     protected abstract void resetCustomer();
@@ -45,96 +62,131 @@ public abstract class VillagerEntityMixin {
     private void captureMob(EntityType entityType, World world, VillagerType type, CallbackInfo ci) {
         villager = (VillagerEntity) (Object) this;
         isInTradingHall = false;
+        shouldRefreshTradingHall = false;
         randomSelectedTick = villager.getId();
+        cachedProfessionEntry = null;
+        reusableSide = new BlockPos.Mutable();
+    }
+
+    @Override
+    public void lazyai$invalidateBlockCache(BlockPos pos) {
+        long key = pos.asLong();
+        if (cachedBlockPos.containsKey(key)) {
+            cachedBlockPos.remove(key);
+            shouldRefreshTradingHall = true;
+        }
     }
 
     @Inject(method = "mobTick", at = @At("HEAD"), cancellable = true)
     private void skipIdleTradingHallTick(CallbackInfo ci) {
-        if (villager != null) {
-            if (!villager.isAlive() || villager.isBaby() || isPanicking()) {
-                return;
-            }
+        if (villager == null || !villager.isAlive() || villager.isBaby() || isPanicking()) return;
+        if (!isInTradingCell(villager)) return;
 
-            if (isInTradingCell(villager)) {
+        VillagerProfession villagerprof = getCachedProfession();
+        if (villagerprof == VillagerProfession.NONE || villagerprof == VillagerProfession.NITWIT) return;
 
-                VillagerProfession villagerprof = villager.getVillagerData().getProfession();
-                if (villagerprof == VillagerProfession.NONE || villagerprof == VillagerProfession.NITWIT) {
-                    return;
-                }
+        if (villager.hasCustomer()) return;
 
-                if (villager.hasCustomer()) {
-                    return;
-                }
+        if ((villager.age + randomSelectedTick) % 20 != 0) {
+            VillagerEntityAccessor accessor = (VillagerEntityAccessor) villager;
 
-                if (!villager.getNavigation().isIdle()) {
-                    return;
-                }
+            int tempInt = accessor.getLevelUpTimer();
+            if (!villager.hasCustomer() && tempInt > 0) {
+                accessor.setLevelUpTimer(tempInt - 1);
 
-                if ((villager.age + randomSelectedTick) % 20 != 0) {
-                    VillagerEntityAccessor accessor = (VillagerEntityAccessor) villager;
-
-                    int tempInt = accessor.getLevelUpTimer();
-                    if (!villager.hasCustomer() && tempInt > 0) {
-                        accessor.setLevelUpTimer(tempInt - 1);
-
-                        if (accessor.getLevelUpTimer() <= 0) {
-                            if (accessor.isLevelingUp()) accessor.invokeLevelUp();
-                            villager.addStatusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 200, 0));
-                        }
-                    }
-
-                    World world = villager.getEntityWorld();
-                    PlayerEntity lastcust = accessor.getLastCustomer();
-                    if (lastcust != null && world instanceof ServerWorld) {
-                        ((ServerWorld) world).handleInteraction(EntityInteraction.TRADE, lastcust, villager);
-                        world.sendEntityStatus(villager, (byte) 14);
-                        accessor.setLastCustomer(null);
-                    }
-
-                    if (villager.getVillagerData().getProfession() == VillagerProfession.NONE && villager.hasCustomer()) {
-                        resetCustomer();
-                    }
-
-                    ci.cancel();
+                if (accessor.getLevelUpTimer() <= 0) {
+                    if (accessor.isLevelingUp()) accessor.invokeLevelUp();
+                    villager.addStatusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 200, 0));
                 }
             }
+
+            PlayerEntity lastcust = accessor.getLastCustomer();
+            if (lastcust != null && villager.getWorld() instanceof ServerWorld world) {
+                (world).handleInteraction(EntityInteraction.TRADE, lastcust, villager);
+                world.sendEntityStatus(villager, (byte) 14);
+                lastcust = null;
+            }
+
+            if (villager.getVillagerData().getProfession() == VillagerProfession.NONE && villager.hasCustomer())
+                resetCustomer();
+
+            ci.cancel();
         }
     }
 
     @Unique
     private boolean isInTradingCell(VillagerEntity villager) {
-        if ((villager.age + randomSelectedTick) % getCooldownList()[Lazy_ai_pixelindiedev.getDistance(villager).ordinal()] != 0) {
-            return isInTradingHall;
-        } else {
-            final BlockPos center = villager.getBlockPos();
+        int[] cooldownList = getCooldownList();
+        int distanceOrdinal = Lazy_ai_pixelindiedev.getDistance(villager).ordinal();
+
+        if ((villager.age + randomSelectedTick) % cooldownList[distanceOrdinal] != 0) return isInTradingHall;
+
+        final BlockPos center = villager.getBlockPos();
+        //if block was changed near, or villager is no longer standing in the same spot
+        if (shouldRefreshTradingHall || lastStandingLocation != center) {
+            shouldRefreshTradingHall = false;
+            lastStandingLocation = center;
+
             final World world = villager.getEntityWorld();
 
-            int blockedDirections = 0;
+            int fullyBlockedDirections = 0;
+            int halfBlockedDirections = 0;
+            for (Direction direction : directionsDirections) {
+                reusableSide.set(center, direction);
+                boolean baseSolid = getCachedSolidBlock(world, reusableSide);
+                reusableSide.move(Direction.UP);
+                boolean upperSolid = getCachedSolidBlock(world, reusableSide);
 
-            if (isDirectionBlocked(world, center.north())) blockedDirections++;
-            if (isDirectionBlocked(world, center.east())) blockedDirections++;
-            if (isDirectionBlocked(world, center.south())) blockedDirections++;
-            if (isDirectionBlocked(world, center.west())) blockedDirections++;
+                if (baseSolid) {
+                    if (upperSolid) fullyBlockedDirections++;
+                    else halfBlockedDirections++;
+                } else {
+                    if (upperSolid) fullyBlockedDirections++;
+                    else return isInTradingHall = false; //open wall found
+                }
+            }
 
-            isInTradingHall = blockedDirections >= 3;
-            return isInTradingHall;
+            //fully closed off
+            if (fullyBlockedDirections >= 4) return isInTradingHall = true;
+
+            if (halfBlockedDirections > 0) {
+                //check ceiling, if villager can jump to get out of hole
+                reusableSide.set(center).move(Direction.UP, 2);
+                return isInTradingHall = getCachedSolidBlock(world, reusableSide);
+            }
+
+            //should not reach this, but just in case
+            return isInTradingHall = false;
+        }
+
+        //trading hall check does not need to refresh, so return the saved value
+        return isInTradingHall;
+    }
+
+    @Unique
+    private VillagerProfession getCachedProfession() {
+        VillagerProfession current = villager.getVillagerData().getProfession();
+        if (current != cachedProfessionEntry) {
+            cachedProfessionEntry = current;
+        }
+        return cachedProfessionEntry;
+    }
+
+    @Unique
+    private boolean getCachedSolidBlock(World world, BlockPos pos) {
+        long key = pos.asLong();
+        if (cachedBlockPos.containsKey(key)) return cachedBlockPos.get(key);
+        else {
+            BlockState state = world.getBlockState(pos);
+            boolean isSolid = hasSolidCollision(state);
+            cachedBlockPos.put(key, isSolid);
+            return isSolid;
         }
     }
 
     @Unique
-    private boolean isDirectionBlocked(World world, BlockPos pos) {
-        return isSolidBlock(world, pos) && isSolidBlock(world, pos.up());
-    }
-
-    @Unique
-    private boolean isSolidBlock(World world, BlockPos pos) {
-        BlockState state = world.getBlockState(pos);
-        return !state.isAir() && state.isSolidBlock(world, pos);
-    }
-
-    @Unique
     private int[] getCooldownList() {
-        return switch (Lazy_ai_pixelindiedev.getOptimalizationType()) {
+        return switch (getOptimalizationType()) {
             case Minimal -> cooldownsMinimal;
             case Agressive -> cooldownsAgressive;
             default -> cooldowns;
@@ -142,7 +194,7 @@ public abstract class VillagerEntityMixin {
     }
 
     @Unique
-    public boolean isPanicking() {
+    private boolean isPanicking() {
         return villager.getBrain().hasActivity(Activity.PANIC);
     }
 }
