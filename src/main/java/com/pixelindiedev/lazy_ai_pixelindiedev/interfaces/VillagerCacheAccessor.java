39f2a8771aa467c90b3980d13738bb732c8a62d2
Pixel <89741457+PixelIndieDev@users.noreply.github.com>
2026-03-13 20:54:14 +0100

    Delete src/main directory

--- src/main/java/com/pixelindiedev/lazy_ai_pixelindiedev/interfaces/VillagerCacheAccessor.java
+++
@@ -1,7 +0,0 @@
-package com.pixelindiedev.lazy_ai_pixelindiedev.interfaces;
-
-import net.minecraft.util.math.BlockPos;
-
-public interface VillagerCacheAccessor {
-    void lazyai$invalidateBlockCache(BlockPos pos);
-}
