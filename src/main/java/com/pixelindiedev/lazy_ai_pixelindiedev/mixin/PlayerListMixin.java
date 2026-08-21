aeada5cb7b40866c95f83a479a0d27c8ce77117b
Pixel <89741457+PixelIndieDev@users.noreply.github.com>
2026-08-21 11:25:30 +0200

    Remove tick simulation distance checks

---
+++ src/main/java/com/pixelindiedev/lazy_ai_pixelindiedev/mixin/PlayerListMixin.java
@@ -0,0 +1,22 @@
+package com.pixelindiedev.lazy_ai_pixelindiedev.mixin;
+
+// LazyAI
+// Copyright (c) 2025 PixelIndieDev
+//
+// Licensed under the GNU GENERAL PUBLIC LICENSE
+// See the LICENSE file in the project root for full license information.
+
+import com.pixelindiedev.lazy_ai_pixelindiedev.config.BlockDistancesHelper;
+import net.minecraft.server.players.PlayerList;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(PlayerList.class)
+public class PlayerListMixin {
+    @Inject(method = "setSimulationDistance", at = @At("TAIL"))
+    private void onSimulationDistanceChanged(int simulationDistance, CallbackInfo ci) {
+        BlockDistancesHelper.SetSimulationDistance(simulationDistance);
+    }
+}
